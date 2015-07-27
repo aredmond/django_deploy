7df5845f591504c21c7f8c13420d98d501836df9
Andrew Redmond <andrewpredmond@gmail.com>
2015-07-27 06:45:12 -0500

    Created cookbook boilerplate

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'django_deploy'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures django_deploy'
+long_description 'Installs/Configures django_deploy'
+version          '0.1.0'
+
